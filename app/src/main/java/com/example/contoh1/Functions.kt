70747ed4d3312044cd81d4bfc61724f7e069cdab
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-10-10 19:00:29 +0700

    Membuat fungsi withNamedArgument dengan variabel name dan age, kemudian fungsi dipanggil di main

--- app/src/main/java/com/example/contoh1/Functions.kt
+++ app/src/main/java/com/example/contoh1/Functions.kt
@@ -11,7 +11,17 @@ fun withParameter(name: String){
     println("Hello, $name!")
 }
 
+//nemed argument
+//named argument merupakan cara untuk memanggil
+fun withNamedArgumrnt(name: String, age: Int){
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name you are $age years old")
+
+}
+
 fun main(){
     withoutParameter()
     withParameter("John")
+    withNamedArgumrnt(name = "john", age = 30)
 }

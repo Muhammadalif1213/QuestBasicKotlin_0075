5eb8cd4f11cd8ad11d8dad6a8430cb8b1542a451
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-10-10 22:50:25 +0700

    membuat 4 macam perulangan: perulangan 1-5, perulangan dari list, perulangan menggunakan while, perulangan menggunakan do-While

--- app/src/main/java/com/example/contoh1/ConditionalExpression.kt
+++ app/src/main/java/com/example/contoh1/ConditionalExpression.kt
@@ -38,7 +38,26 @@ fun Perulangan(){
     println()
     println("=== Perulangan ===")
 
+    for (i in 1..5){
+        println("Perulangan ke-$i")
+    }
 
+    val Cars = listOf("BMW","Toyota","Honda","Audi")
+    for (car in Cars){
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    }while (y <= 5)
 }
 
 fun main(){

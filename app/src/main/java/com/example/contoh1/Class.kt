0b6da6d4edaa46e53b0f54317c001cab6a726e92
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-10-10 20:21:07 +0700

    membuat perintah untuk mendisplay email, melalui class Contact

--- app/src/main/java/com/example/contoh1/Class.kt
+++ app/src/main/java/com/example/contoh1/Class.kt
@@ -6,4 +6,9 @@ class Contact(val id: Int, var email: String)
 
 fun main() {
     val Contact = Contact (1,"mary@gmail.com")
+
+    //print the value of property: email
+    println(Contact.email)
+
+
 }

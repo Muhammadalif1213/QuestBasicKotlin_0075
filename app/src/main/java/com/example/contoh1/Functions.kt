7ad6cab3a906bde84792e618f8d275701de8a622
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-10-10 19:07:46 +0700

    Membuat fungsi dengan parameter yang sudah di tentukan, kemudian dipanggil di main dengan memasukan int yang diperlukan

--- app/src/main/java/com/example/contoh1/Functions.kt
+++ app/src/main/java/com/example/contoh1/Functions.kt
@@ -17,11 +17,19 @@ fun withNamedArgumrnt(name: String, age: Int){
     println()
     println("== withNamedArgument ==")
     println("Hello, $name you are $age years old")
+}
 
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name:String = "Indra", age: Int){
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello $name you are $age years old.")
 }
 
 fun main(){
     withoutParameter()
     withParameter("John")
     withNamedArgumrnt(name = "john", age = 30)
+    withDefaultParameter(age = 25)
 }

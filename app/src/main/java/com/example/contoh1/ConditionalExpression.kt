64d762242eee8c6bf79162974b5829710bf95d46
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-10-10 21:49:39 +0700

    membuat if-else didalam fungsi conditionalStatement, jika diberikan parameter "number" yang memiliki value = 10. akan menampilkan positif jika valuenya > 0 dan negatif jika < 0

--- app/src/main/java/com/example/contoh1/ConditionalExpression.kt
+++ app/src/main/java/com/example/contoh1/ConditionalExpression.kt
@@ -2,7 +2,16 @@ package com.example.contoh1
 
 fun conditionalStatement(){
     println("=== Conditional Expression ===")
+
+    //if else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    }else{
+        println("Negative Number")
+    }
 }
+
 
 fun main(){
     conditionalStatement()
